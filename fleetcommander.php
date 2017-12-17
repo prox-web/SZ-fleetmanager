5e747d99d3adcd2772f9ed9d90e056d040935f60
Marcel Santing <root>
2017-12-17 14:28:48 +0100

    Starting the first steps of the plugin

---
+++ fleetcommander.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *	Plugin Name: Star Citizen fleet commander alpha
+ *  Author: Prox-Web
+ *  Licence: GNU-3
+ *  Version: In Development
+ * Description: The plugin for Wordpress to help organize you fleet, commodities, members and much more
+ *
+ */
